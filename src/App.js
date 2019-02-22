bf41b493adc267e6098e7b986c93c858f8a80369
vinoth kumar k <smartvinothkumar.k@gmail.com>
2019-02-22 15:16:35 +0530

    My First React App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = ()=>(<PersonList />)
+
+const PersonList = () =>{
+
+  const people = [
+    {
+      img:22, name:"Vinoth", Job:"Google CEO"
+    },
+    {
+      img:55, name:"Aravind", Job:"Yahoo CEO"
+    },
+    {
+      img:75, name:"Raj Kumar", Job:"Dell CEO"
+    }
+  ]
+  return(
+    <section>
+       <Person person = {people[0]}/>;
+       <Person person = {people[1]}>
+        He is CEO of Yahoo and he made lot of changes to Yahoo company.
+        He offering jobs like average student and make him/her to better feature grouth
+       </Person>;
+       <Person person = {people[2]}/>;
+    </section>
+  );
+} 
+
+const Person = (props) =>{
+
+  const { img, name, Job } = props.person;
+  const { children } = props;
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+  return( 
+    <div className="person">
+      <img src={url} alt=""/>
+        <div>
+          <h4>{name}</h4>
+          <h4>{Job}</h4>
+          {children}
+        </div>
+    </div>
+  );
 }
+
 
 export default App;
